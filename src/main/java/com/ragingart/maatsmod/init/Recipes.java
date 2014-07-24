6cacd88764a0db257143b16ad946e102ebe6879a
Ragingart <sleepymaat@gmx.de>
2014-07-24 17:33:18 +0200

    References for Recipes (Component)

--- src/main/java/com/ragingart/maatsmod/init/Recipes.java
+++ src/main/java/com/ragingart/maatsmod/init/Recipes.java
@@ -1,7 +1,7 @@
 package com.ragingart.maatsmod.init;
 
+import com.ragingart.maatsmod.ref.Component;
 import cpw.mods.fml.common.registry.GameRegistry;
-import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.oredict.ShapedOreRecipe;
 
@@ -9,7 +9,7 @@ public class Recipes {
 
     public static void init()
     {
-        GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(ModItems.multitool),"r d"," s ","s  ",'s',"stickWood",'r',new ItemStack(ModItems.battery),'d',"gemDiamond"));
-        GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(ModItems.battery)," g ","iri","iri",'g',"nuggetGold",'i',"ingotIron",'r',"dustRedstone"));
+        GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(ModItems.multitool),"r d"," s ","s  ",'s', Component.HANDLE,'r',new ItemStack(ModItems.battery),'d',"gemDiamond"));
+        GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(ModItems.battery)," g ","iri","iri",'g', Component.SMALL_GOLD,'i', Component.NORMAL_IRON,'r',"dustRedstone"));
     }
 }
---
+++ src/main/java/com/ragingart/maatsmod/ref/Component.java
@@ -0,0 +1,9 @@
+package com.ragingart.maatsmod.ref;
+
+
+public class Component {
+    // OreDictionary Names for Recipe Components
+    public static final String SMALL_GOLD = "nuggetGold";
+    public static final String NORMAL_IRON = "ingotIron";
+    public static final String HANDLE = "stickWood";
+}
