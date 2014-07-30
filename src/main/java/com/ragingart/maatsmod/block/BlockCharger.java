ee14c37c89eba0f73c34b5c797e4272e12cbea8b
Ragingart <sleepymaat@gmx.de>
2014-07-30 14:35:06 +0200

    something fluxi

--- src/main/java/com/ragingart/maatsmod/block/BlockCharger.java
+++ src/main/java/com/ragingart/maatsmod/block/BlockCharger.java
@@ -2,12 +2,49 @@ package com.ragingart.maatsmod.block;
 
 
 import com.ragingart.maatsmod.generics.BlockMM;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.block.ITileEntityProvider;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.IIcon;
+import net.minecraft.world.World;
 
-public class BlockCharger extends BlockMM {
+public class BlockCharger extends BlockMM implements ITileEntityProvider{
+    @SideOnly(Side.CLIENT)
+    private IIcon front_txt_on;
+    @SideOnly(Side.CLIENT)
+    private IIcon front_txt_off;
+
+
        public BlockCharger()
        {
            super("charger");
            this.setHardness(7.0F);
            this.setHarvestLevel("wrench",4);
        }
+
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public IIcon getIcon(int side,int meta)
+    {
+        if(side==1) return front_txt_on;
+        if(side==2) return front_txt_off;
+        return blockIcon;
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void registerBlockIcons(IIconRegister iconRegister)
+    {
+        blockIcon = iconRegister.registerIcon("maatsmod:casing");
+        front_txt_on = iconRegister.registerIcon("maatsmod:charger_front_on");
+        front_txt_off = iconRegister.registerIcon("maatsmod:charger_front_off");
+    }
+
+    @Override
+    public TileEntity createNewTileEntity(World var1, int var2) {
+        return null;
+    }
 }
--- src/main/java/com/ragingart/maatsmod/item/ItemMultitool.java
+++ src/main/java/com/ragingart/maatsmod/item/ItemMultitool.java
@@ -1,21 +1,26 @@
 package com.ragingart.maatsmod.item;
 
 
+import cofh.api.energy.IEnergyContainerItem;
 import com.google.common.collect.Sets;
 import com.ragingart.maatsmod.block.BlockCharger;
-import com.ragingart.maatsmod.generics.BlockMM;
 import com.ragingart.maatsmod.generics.ItemToolMM;
 import com.ragingart.maatsmod.init.ModBlocks;
+import com.ragingart.maatsmod.util.LogHelper;
 import net.minecraft.block.Block;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.world.World;
 
 import java.util.Set;
 
-public class ItemMultitool extends ItemToolMM
+public class ItemMultitool extends ItemToolMM implements IEnergyContainerItem
 {
     private static final Set blocksEffectiveAgainst = Sets.newHashSet(new Block[]{ModBlocks.Ore,ModBlocks.Charger});
+    private int cap;
+    private int maxIn;
+    private int maxOut;
 
 
     public ItemMultitool()
@@ -24,6 +29,9 @@ public class ItemMultitool extends ItemToolMM
         this.setHarvestLevel("pickaxe",3);
         this.setHarvestLevel("wrench",4);
         this.setUnlocalizedName("multitool");
+        this.cap=3000;
+        this.maxIn=100;
+        this.maxOut=25;
     }
 
 
@@ -42,8 +50,9 @@ public class ItemMultitool extends ItemToolMM
         System.out.println(par4 + " / " + par5 + " / " + par6 + " / " + par7);
         Block block = par3World.getBlock(par4,par5,par6);
         if (block instanceof BlockCharger){
-            block.dropBlockAsItem(par3World,par4,par5,par6,0,1);
+            //block.dropBlockAsItem(par3World,par4,par5,par6,0,1);
         }
+        LogHelper.info("Energy:" + getEnergyStored(par1ItemStack));
         return true;
     }
 
@@ -51,7 +60,53 @@ public class ItemMultitool extends ItemToolMM
     public ItemStack onItemRightClick(ItemStack par1ItemStack, World par2World, EntityPlayer par3EntityPlayer)
     {
         System.out.println("rightclick on air");
+        receiveEnergy(par1ItemStack,10,false);
         return par1ItemStack;
     }
 
+    @Override
+    public int receiveEnergy(ItemStack container, int maxReceive, boolean simulate) {
+
+        if (container.stackTagCompound == null) {
+            container.stackTagCompound = new NBTTagCompound();
+        }
+        int energy = container.stackTagCompound.getInteger("Energy");
+        int energyReceived = Math.min(cap - energy, Math.min(this.maxIn, maxReceive));
+
+        if (!simulate) {
+            energy += energyReceived;
+            container.stackTagCompound.setInteger("Energy", energy);
+        }
+        return energyReceived;
+    }
+
+    @Override
+    public int extractEnergy(ItemStack container, int maxExtract, boolean simulate) {
+
+        if (container.stackTagCompound == null || !container.stackTagCompound.hasKey("Energy")) {
+            return 0;
+        }
+        int energy = container.stackTagCompound.getInteger("Energy");
+        int energyExtracted = Math.min(energy, Math.min(this.maxOut, maxExtract));
+
+        if (!simulate) {
+            energy -= energyExtracted;
+            container.stackTagCompound.setInteger("Energy", energy);
+        }
+        return energyExtracted;
+    }
+
+    @Override
+    public int getEnergyStored(ItemStack container) {
+
+        if (container.stackTagCompound == null || !container.stackTagCompound.hasKey("Energy")) {
+            return 0;
+        }
+        return container.stackTagCompound.getInteger("Energy");
+    }
+
+    @Override
+    public int getMaxEnergyStored(ItemStack container) {
+        return cap;
+    }
 }
