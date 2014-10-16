4339e4a7c02a9dd03b9c5f82f0fef79e70537c3c
WurstSchnitte <wert369369>
2014-10-16 10:08:06 +0200

    Water Turbine final version. Both Bugs Fixed + Works nearly Perfect

--- src/main/java/com/ragingart/maatsmod/tileentity/TileEntityWaterTurbine.java
+++ src/main/java/com/ragingart/maatsmod/tileentity/TileEntityWaterTurbine.java
@@ -16,36 +16,48 @@ public class TileEntityWaterTurbine extends TileEntityEnergyGen {
         tank.setCapacity(1000);
     }
 
-
     @Override
     public void updateEntity()
     {
         super.updateEntity();
         if(!worldObj.isRemote){
             checkWaterFlow();
+            isAktive();
         }
     }
 
-
+    private void isAktive(){
+        int y_down = this.yCoord + ForgeDirection.DOWN.offsetY;
+        Block aBlock = worldObj.getBlock(xCoord, y_down, zCoord);
+        int aMeta = worldObj.getBlockMetadata(xCoord, y_down, zCoord);
+        if(tank.getFluidAmount() != 0 && tank.getFluid().getFluid().getID() == Fluids.ID.HIGHHELDWATER.ordinal()) {
+            if (machineHelper.hasPort(0, CasingHelper.Port.FOUTPUT) && (aBlock.getMaterial() == Material.air || aBlock.getMaterial() == Material.water)) {
+                machineHelper.setState(2);
+                worldObj.setBlock(xCoord, y_down, zCoord, Blocks.flowing_water);
+                worldObj.markBlockForUpdate(xCoord, y_down, zCoord);
+                energy.receiveEnergy(tank.drain(1000, true).amount, false);
+            } else {
+                machineHelper.setState(1);
+                if (aBlock.getMaterial() == Material.water && aMeta == 0) {
+                    worldObj.setBlockToAir(xCoord, y_down, zCoord);
+                }
+            }
+        }
+        else{
+            machineHelper.setState(0);
+            if (aBlock.getMaterial() == Material.water && aMeta == 0) {
+                worldObj.setBlockToAir(xCoord, y_down, zCoord);
+            }
+        }
+    }
 
     private void checkWaterFlow(){
         int x = this.xCoord;
         int z = this.zCoord;
         int y_up = this.yCoord + ForgeDirection.UP.offsetY;
-        int y_down = this.yCoord + ForgeDirection.DOWN.offsetY;
-
-
         Block aBlock = worldObj.getBlock(x, y_up, z);
         int aMeta = worldObj.getBlockMetadata(x, y_up, z);
-
-        if(aBlock.getMaterial() != Material.water && aMeta != 8) {
-            machineHelper.setState(0);
-            // Bug 1: Wenn Machine Outputvalve hat und läuft und Output valve dann getauscht wird auf blank wird wasserblock nicht entfernt
-            // Bug 2: Wenn 2 Turbinen nebeneinander sind #Erklärung
-            if(worldObj.getBlock(x, y_down, z).getMaterial() == Material.water) // TODO this no good
-                worldObj.setBlockToAir(x, y_down, z);
-
-        } else if(machineHelper.hasPort(1,CasingHelper.Port.FINPUT) && aMeta == 8  && aBlock.getMaterial()== Material.water && tank.getFluidAmount() == 0) {
+        if(machineHelper.hasPort(1,CasingHelper.Port.FINPUT) && aMeta >= 8  && aBlock.getMaterial()== Material.water && tank.getFluidAmount() == 0) {
             machineHelper.setState(1);
             int new_y_up = y_up;
 
@@ -54,26 +66,15 @@ public class TileEntityWaterTurbine extends TileEntityEnergyGen {
                 new_y_up += ForgeDirection.UP.offsetY;
                 aBlock = worldObj.getBlock(x, new_y_up, z);
                 aMeta = worldObj.getBlockMetadata(x, new_y_up, z);
-                if(aMeta == 8  && aBlock.getMaterial()== Material.water)
+                if(aMeta < 8  || aBlock.getMaterial()!= Material.water)
                 {
-                    waterAbove++;
                     break;
                 }
+                waterAbove++;
             }
             tank.fill(new FluidStack(Fluids.ID.HIGHHELDWATER.ordinal(), waterAbove), true);
-            //LogHelper.info(waterAbove);
-        }
-        if(machineHelper.hasPort(0, CasingHelper.Port.FOUTPUT) && tank.getFluidAmount() != 0 && tank.getFluid().getFluid().getID() == Fluids.ID.HIGHHELDWATER.ordinal()){
-            aBlock = worldObj.getBlock(x, y_down, z);
-            if(aBlock.getMaterial() == Material.air || aBlock.getMaterial() == Material.water) {
-                machineHelper.setState(2);
-                worldObj.setBlock(x, y_down, z, Blocks.flowing_water);
-                worldObj.markBlockForUpdate(x,y_down,z);
-                energy.receiveEnergy(tank.drain(1000, true).amount, false);
-            }
         }
     }
-
 
     @Override
     public int[] validPorts() {
